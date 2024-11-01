86e5c37bd47c12a85b97b54c45567aec2a5b33e0
TaDungg <trongdung.work@gmail.com>
2024-11-01 11:03:22 +0700

    Add files via upload

---
+++ BTL-Web-TaDungg/assets/js/home.js
@@ -0,0 +1,44 @@
+
+let slideIndex = 1;
+let slideInterval;
+
+function showSlides(n) {
+    const slides = document.getElementsByClassName("slide");
+    const dots = document.getElementsByClassName("dot");
+    
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active-dot", "");
+    }
+    
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active-dot";
+}
+
+function changeSlide(n) {
+    clearInterval(slideInterval);
+    showSlides(slideIndex += n);
+    startAutoSlide();
+}
+
+function currentSlide(n) {
+    clearInterval(slideInterval);
+    showSlides(slideIndex = n);
+    startAutoSlide();
+}
+
+function startAutoSlide() {
+    slideInterval = setInterval(() => {
+        slideIndex++;
+        showSlides(slideIndex);
+    }, 5000); // Chuyển slide sau mỗi 5 giây
+}
+
+// Khởi tạo slideshow
+showSlides(slideIndex);
+startAutoSlide();
